6334107e56d5c284a0c5a80eb594b09fd53a1143
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2023-09-14 03:20:16 +0530

    Added UI

---
+++ Productpricecompare/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Productpricecompare project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Productpricecompare.settings')
+
+application = get_wsgi_application()
